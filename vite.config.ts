28c80e773b508f555a4720db6b304df7c339cbb3
Simone Rossi <ro.saimon@gmail.com>
2026-03-29 10:57:56 +0200

    Update generale

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: mode === 'production' ? '/manoy-s-event-beats/' : '/',
+  base: '/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
